c3482cde4b99905479ba3920ce841958c029d94c
Sakhile Mamba <sakhymamba@gmail.com>
2026-07-02 08:34:44 +0200

    Week1

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,20 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        /// 1. Create a new array to hold the results. It's capacity must be the length parameter
+        /// 2. Create a new for loop to with length parameter number of steps
+        /// 3. Inside the for loop, in each step, for each index in the results array you created, as per the value of the loop variable i, insert a value of number parameter multiplied by (i + 1)
 
-        return []; // replace this return statement with your own
+        // Create a new array
+        var results = new double[length];
+
+        // Loop through the length of the array
+        for (int i = 0; i < length; i++)
+        {
+            // For each index in the array result as per the value of the loop variable i, insert a value of number multiplied by (i + 1)
+            results[i] = number * (i + 1);
+        }
+        return results;
     }
 
     /// <summary>
@@ -25,9 +33,16 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        /// 1. Create a new for loop that as number of steps equal to amount parameter which is the number of elements to rotate in the loop. 
+        /// 2. Inside the loop, take the last element inside the data list parameter and insert it into the first index of the list by copying it. Use list method "Insert"
+        /// 3. Delete the original element at the end of the list. Use list method "RemoveAt"
+        /// 4. Repeat until you have rotate amount parameter number of elements in the data list parameter
+
+
+        for (int i = 0; i < amount; i++)
+        {
+            data.Insert(0, data[data.Count - 1]);
+            data.RemoveAt(data.Count - 1);
+        }
     }
 }
--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -3,14 +3,34 @@ public static class ArraySelector
     public static void Run()
     {
         var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10};
-        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var l2 = new[] { 2, 4, 6, 8, 10 };
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 };
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        var work = new int[select.Length];
+        Console.WriteLine(work.Length);
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        foreach (int i in select)
+        {
+            if (i == 1)
+            {
+                work[z] = (list1[x]);
+                x++;
+                z++;
+            }
+            else if (i == 2)
+            {
+                work[z] = (list2[y]);
+                y++;
+                z++;
+            }
+        }
+        return work;
     }
 }
